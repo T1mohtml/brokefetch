7bf6acf119fa59fb40819eaf8dd2e743bfa7558b
Szerwigi1410 <szerwigi14@gmail.com>
2025-08-11 09:48:37 +0200

    Update brokefetch_EDGE_AC.sh

--- brokefetch_EDGE_AC.sh
+++ brokefetch_EDGE_AC.sh
@@ -753,15 +753,15 @@ done
 
 # If color mode is enabled, set the color variables
 if [ "$COLOR_MODE" = true ]; then
-    GREEN="\033[32m"
-    RED="\033[31m"
-    BLUE="\033[34m"
-    CYAN="\033[36m"
-    WHITE="\033[37m"
-    YELLOW="\033[33m"
-    PURPLE="\033[35m"
-    BOLD="\033[1m"
-    RESET="\033[0m"
+    GREEN=$'\033[32m'
+    RED=$'\033[31m'
+    BLUE=$'\033[34m'
+    CYAN=$'\033[36m'
+    WHITE=$'\033[37m'
+    YELLOW=$'\033[33m'
+    PURPLE=$'\033[35m'
+    BOLD=$'\033[1m'
+    RESET=$'\033[0m'
 fi
 
 # Reset ASCII variables before assigning
